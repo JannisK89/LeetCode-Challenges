8e02d4a6649b92256ce38e4b1ad958848401d7ed
Jannis K <Jannis.Karanikis1@gmail.com>
2020-12-18 19:02:43 +0100

    Initial

---
+++ CSharp/1678 - Goal Parser Interpretation/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://leetcode.com/problems/goal-parser-interpretation/
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. 
+The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+*/
+
+namespace _1678
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Solution();
+
+            System.Console.WriteLine(test.Interpret("G()(al)"));
+            System.Console.WriteLine(test.Interpret("()G()()()()()(al)()()(al)"));
+        }
+    }
+
+    public class Solution
+    {
+        public string Interpret(string command)
+        {
+            command = command.Replace("()", "o").Replace("(al)", ("al"));
+
+            return command;
+        }
+    }
+}
---
+++ CSharp/1684 - Count the Number of Consistent Strings/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+
+Constraints:
+1 <= words.length <= 10^4
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+
+*/
+
+using System;
+
+namespace _1684
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Solution();
+            
+            System.Console.WriteLine(test.CountConsistentStrings("rowd", new string[] { "west", "word", "sword", "r", "doooowwwwrrww" }));
+        }
+    }
+
+    public class Solution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int result = 0;
+            int counter = 0;
+
+            foreach (var word in words)
+            {
+                counter = 0;
+
+                foreach (var letter in word)
+                {
+
+                    if (!allowed.Contains(letter))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == word.Length)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
